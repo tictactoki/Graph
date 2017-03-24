0bcb043d2494021f6a5f337f432e3c90af9cb355
Stéphane Wong <stephane.wong1@gmail.com>
2017-03-24 12:16:42 +0100

    bfs and dfs algo

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -6,31 +6,62 @@ object Node {
 
 
   def BFS[T](node: Node[T]) = {
+    // color 0 for white, 1 for grey and 2 for black
+    val map = scala.collection.mutable.HashMap[Node[T], Int]()
     val queue = scala.collection.mutable.Queue[Node[T]]()
+    val finalQueue = scala.collection.mutable.Queue[Node[T]]()
     queue.enqueue(node)
 
-    def aux(node: List[List[Node[T]]], nextLayer: List[Node[T]]): List[Node[T]] = nextLayer match {
-      case Nil => node.reverse.flatten
-      case _ => aux(nextLayer :: node, nextLayer.map())
+    def fillColor(node: Node[T]): Unit = {
+      map.put(node, (0))
+      node.nodes.map(fillColor)
     }
 
-    val size = queue.size
-    while(!queue.isEmpty){
-      println(queue.dequeue())
+    def getColor(node: Node[T]) = map.getOrElse(node, 0)
+
+    def aux(queue: mutable.Queue[Node[T]]): Unit = {
+      if (queue.isEmpty) return
+      else {
+        val node = queue.dequeue()
+        //white
+        if (getColor(node) == 0) {
+          map.update(node, (1))
+          queue.enqueue(node)
+          aux(queue)
+        }
+        //grey
+        else if (getColor(node) == 1) {
+          map.update(node, (2))
+          queue.enqueue(node)
+          aux(queue)
+        }
+        // black
+        else {
+          node.nodes.foreach(queue.enqueue(_))
+          finalQueue.enqueue(node)
+          aux(queue)
+        }
+      }
     }
 
-    size
+    def displayQueue = finalQueue.map(n => println(n.value))
+
+    fillColor(node)
+    aux(queue)
+    finalQueue.map(_.value)
   }
 
 
   def DFS[T](node: Node[T]) = {
-    def aux(node: Node[T], list: List[Node[T]]): List[Node[T]] = {
-      println(node)
-      node.nodes.flatMap(node => aux(node, node :: list)).toList
-    }
-    val list = aux(node, Nil)
 
-  list.foreach(n => println(n))
+    val stack = scala.collection.mutable.Stack[Node[T]]()
+
+    def aux(node: Node[T]): Unit = {
+      stack.push(node)
+      node.nodes.map(n => aux(n))
+    }
+    aux(node)
+    stack.map(_.value).reverse
 
   }
 
--- src/test/scala/GraphSpec.scala
+++ src/test/scala/GraphSpec.scala
@@ -8,13 +8,16 @@ class GraphSpec extends FlatSpec with Matchers {
   type T = Int
 
   val nodes = Node(1, List(Node(2, List(Node(3, List(Node(4, Nil))), Node(5, Nil), Node(6, List(Node(7, Nil), Node(8, Nil), Node(9, List(Node(10, Nil), Node(11, List(Node(12, Nil))))))))), Node(13, Nil), Node(14, List(Node(15, Nil)))))
+  val bfs = List(1,2,13,14,3,5,6,15,4,7,8,9,10,11,12)
+  val dfs = List(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+
 
   "A BFS" should "display the right value on bfs order" in {
-    Node.BFS(nodes) should equal(15)
+    Node.BFS(nodes) should equal(bfs)
   }
 
   "A DFS" should "display the rigth value on the dfs order" in {
-    Node.DFS(nodes)
+    Node.DFS(nodes) should equal (dfs)
   }
 
 }
